9688c232b07d11658afc474b1211a044ce5ed3ce
james7132 <james.siqi.liu@gmail.com>
2018-01-24 04:44:35 -0800

    Fix Shared not being static

--- src/Runtime/Pooling/ObjectPool.cs
+++ src/Runtime/Pooling/ObjectPool.cs
@@ -3,7 +3,7 @@
 public class ObjectPool<T> : AbstractPool<T> where T : new() {
 
   static ObjectPool<T> sharedIntstance;
-  public ObjectPool<T> Shared => sharedIntstance ?? (sharedIntstance = new ObjectPool<T>());
+  public static ObjectPool<T> Shared => sharedIntstance ?? (sharedIntstance = new ObjectPool<T>());
 
   protected override T CreateNew() => new T();
 

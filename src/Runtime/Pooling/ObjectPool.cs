a84fa46de352391dcece966eff4e1968f47bdd20
james7132 <contact@jamessliu.com>
2018-01-23 20:02:12 -0800

    Add more pools

---
+++ src/Runtime/Pooling/ObjectPool.cs
@@ -0,0 +1,12 @@
+﻿namespace HouraiTeahouse {
+
+public class ObjectPool<T> : AbstractPool<T> where T : new() {
+
+  static ObjectPool<T> sharedIntstance;
+  public ObjectPool<T> Shared => sharedIntstance ?? (sharedIntstance = new ObjectPool<T>());
+
+  protected override T CreateNew() => new T();
+
+}
+
+}

d75abcc2913f1aed4c47cb4efeb38352b23b7f95
james7132 <contact@jamessliu.com>
2018-01-27 15:36:32 -0800

    Add EnumerableExtensions

---
+++ src/Runtime/IEnumerableExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace HouraiTeahouse {
+
+public static class IEnumerableExtensions {
+
+  class ReadOnlyCollection<T> : IReadOnlyCollection<T> {
+
+    readonly IEnumerable<T> Collection;
+
+    public ReadOnlyCollection(IEnumerable<T> collection) {
+      Collection = collection;
+    }
+
+    public int Count => Collection.Count();
+
+    public IEnumerator<T> GetEnumerator() => Collection.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => Collection.GetEnumerator();
+
+  }
+
+  public static IReadOnlyCollection<T> AsReadOnly<T>(this IEnumerable<T> collection) {
+    return new ReadOnlyCollection<T>(Argument.NotNull(collection));
+  }
+
+}
+
+}
